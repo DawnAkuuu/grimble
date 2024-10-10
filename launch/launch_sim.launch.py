1e19b906c0e739207b235cf79dd92743fba009ca
DawnAkuuu <luissilvas12411@gmail.com>
2024-10-09 17:35:54 -0700

    Fixed Gazebo with updated version (Gazebo Harmonic) (Dude this took me HOURS this is insane)

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -10,7 +10,6 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 
-
 def generate_launch_description():
 
 
@@ -33,11 +32,18 @@ def generate_launch_description():
     )
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
-    gazebo = IncludeLaunchDescription(
+    gzserver_cmd = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
-                    launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown': 'true'}.items()
+                    launch_arguments={'gz_args': ['-r -s -v4 ', world], 'on_exit_shutdown': 'true'}.items()
              )
+    get_package_share_directory('ros_gz_sim')
+    gzclient_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments={'gz_args': '-g -v4 '}.items()
+    )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='ros_gz_sim',
@@ -47,12 +53,31 @@ def generate_launch_description():
             '-topic', "robot_description"
         ],
         output='screen',)
+    
 
+    bridge_params = os.path.join(
+        get_package_share_directory(package_name),
+        'params',
+        'turtlebot3_waffle_bridge.yaml'
+    )
 
-
+    start_gazebo_ros_bridge_cmd = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '--ros-args',
+            '-p',
+            f'config_file:={bridge_params}',
+        ],
+        output='screen',
+    )
+    
+    
     # Launch them all!
     return LaunchDescription([
         rsp,
-        gazebo,
+        gzserver_cmd,
+        gzclient_cmd,
         spawn_entity,
+        start_gazebo_ros_bridge_cmd,
     ])
